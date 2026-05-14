1923ad2dfe01012920b8104ba3d419638890ad7c
Muhammad Subkha <muhammadsubkha6@gmail.com>
2026-05-14 19:12:33 +0700

    Add files via upload

---
+++ laravelabsen.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Bulan Mei 2026 pada 14.12
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravelabsen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `absens`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.absens: #1932 - Table &#039;laravelabsen.absens&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.absens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`absens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.failed_jobs: #1932 - Table &#039;laravelabsen.failed_jobs&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jabatan`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.jabatan: #1932 - Table &#039;laravelabsen.jabatan&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.jabatan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`jabatan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.migrations: #1932 - Table &#039;laravelabsen.migrations&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_reset_tokens`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.password_reset_tokens: #1932 - Table &#039;laravelabsen.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengajuan_izin`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.pengajuan_izin: #1932 - Table &#039;laravelabsen.pengajuan_izin&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.pengajuan_izin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`pengajuan_izin`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.personal_access_tokens: #1932 - Table &#039;laravelabsen.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+-- Kesalahan membaca struktur untuk tabel laravelabsen.users: #1932 - Table &#039;laravelabsen.users&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel laravelabsen.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `laravelabsen`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
